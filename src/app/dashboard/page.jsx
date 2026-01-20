41685e95bcea483c29a0ae28730a1c4683d8ca52
ronakbhalara-lab <ronakbhalara@gmail.com>
2026-01-20 17:43:21 +0530

    chnages

--- src/app/dashboard/page.jsx
+++ src/app/dashboard/page.jsx
@@ -647,7 +647,7 @@ export default function Dashboard() {
 
           {/* Main Content */}
           <div className="flex-1 lg:ml-0">
-            <div className="p-4 sm:p-6 lg:p-8">
+            <div className="p-4 sm:p-6 lg:p-4">
               {/* Header */}
               <div className="mb-6 lg:mb-8">
                 <div className="flex flex-col sm:flex-row sm:items-center sm:justify-between">
